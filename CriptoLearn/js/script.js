5da1ff5ed983a4d8917058cbc95a34caa788fe1a
Ícaro <icaro.o.freitas2005@gmail.com>
2025-05-04 23:20:14 -0300

    Add files via upload

---
+++ CriptoLearn/js/script.js
@@ -0,0 +1,5 @@
+// Simula carregamento de nível
+document.addEventListener("DOMContentLoaded", () => {
+    const progress = document.querySelector(".progress-fill");
+    progress.style.width = "80%"; // Simula 80% do level 5
+  });
